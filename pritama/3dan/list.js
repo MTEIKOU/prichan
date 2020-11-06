5b98aacb84e9f6742b2f02de1fc6e323692f68c5
TEIKOU <yoshiki444.jp@gmail.com>
2020-11-06 17:21:19 +0900

    1106

--- pritama/3dan/list.js
+++ pritama/3dan/list.js
@@ -1,3 +1,22 @@
-function imgClick() {
-  alert("クリックされました");
+function imgClick(select) {
+  select.bgColor="gray";
 }
+
+function select(itemid) {
+  localstorage.setItem("itemid","1")
+}
+
+const er =[
+  [Item_ID72513.png,Item_ID72511.png,Item_ID72512.png,],
+  [Item_ID72516.png,Item_ID72514.png,Item_ID72515.png,],
+];
+
+const fr =[
+  [Item_ID72519.png,Item_ID72517.png,Item_ID72518.png,],
+  [Item_ID72522.png,Item_ID72520.png,Item_ID72521.png,],
+];
+
+const pr =[
+  [Item_ID72526.png,Item_ID72523.png,Item_ID72524.png,Item_ID72525.png],
+  [Item_ID72530.png,Item_ID72527.png,Item_ID72528.png,Item_ID72529.png],
+];
