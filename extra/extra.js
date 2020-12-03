d6bb0291b6773114da6650f860763923887c7380
TEIKOU <yoshiki444.jp@gmail.com>
2020-12-03 17:30:52 +0900

    1203

--- extra/extra.js
+++ extra/extra.js
@@ -1,25 +1,26 @@
 
 function Random(p1){
 
-  let p2 = Math.floor(Math.random() * Math.floor(3));
+  let p2 = Math.floor(Math.random() * 6);
   let result='';
-  let e=hand(p2);
-
+  let e=hand(p2%3);
+  console.log(p2%3);
   switch((p1-p2+3)%3){
     case 0:
-    result += 'あいこ';
+    result = 'あいこ';
     break;
     case 1:
-    result += '負け…';
+    result = '負け…';
     break;
     case 2:
-    result += '勝ち!';
+    result = '勝ち!';
     break;
   }
 
-  console.log("")
-  document.getElementById("hand").innerText=e;
-  document.getElementById("result").innerText=result;
+
+  document.getElementById("image").innerHTML="<img src='" + (p2+1) + ".png'>";
+  document.getElementById("hand").innerHTML=e;
+  document.getElementById("result").innerHTML=result;
 }
 
 function hand(input){
