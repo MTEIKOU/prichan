6c8446c4b38e6283577cd88651fde56055d4f03e
TEIKOU <yoshiki444.jp@gmail.com>
2020-12-08 11:04:42 +0900

    調整

--- extra/extra.js
+++ extra/extra.js
@@ -1,27 +1,37 @@
+let offset = 1;
 
 function Random(p1){
 
-  let p2 = Math.floor(Math.random() * 6);
+  let p2 = Math.floor(Math.random() * 3);
   let result='';
-  let e=hand(p2%3);
+  //let e=hand(p2%3);
 
-  //console.log(p1 +'-'+p2+'+3%3='+((p1-p2+6)%3));
-  switch((p1-p2+6)%3){
+  //switch((p1-p2+6)%3)
+  switch((p1-p2+3)%3){
     case 0:
     result = 'あいこ';
+    document.getElementById("result").classList.remove("red");
+    document.getElementById("result").classList.remove("blue");
     break;
     case 1:
     result = '負け…';
+    document.getElementById("result").classList.remove("red");
+    document.getElementById("result").classList.add("blue");
     break;
     case 2:
     result = '勝ち!';
+    document.getElementById("result").classList.add("red");
+    document.getElementById("result").classList.remove("blue");
     break;
   }
 
-
-  document.getElementById("image").innerHTML="<img src='" + (p2+1) + ".png'>";
-  document.getElementById("hand").innerHTML=e;
+  document.getElementById("img").innerHTML="<img class='image' src='" + (p2 + offset) + ".png'>";
+  //document.getElementById("hand").innerHTML=e;
   document.getElementById("result").innerHTML=result;
+
+  if(offset==1){offset=4}
+  else{offset=1}
+
 }
 
 function hand(input){
