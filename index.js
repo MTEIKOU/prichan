9941b5f1ffc2d6eacab92e542df4c7c3a80b3ec3
TEIKOU <yoshiki444.jp@gmail.com>
2020-12-23 16:59:33 +0900

    index layout

--- index.js
+++ index.js
@@ -11,11 +11,8 @@ location.reload();
 */ //All Reset
 
 function schedule(){
-console.log(day);
   for(let i=0; i<day.length; i++){
-  let test =''
-  text = kikan(day[i][0]) + "～" + kikan(day[i][1]);
-  console.log(i +1);
+  const text = kikan(day[i][0]) + "～" + kikan(day[i][1]);
   document.getElementById((i+1)+'dan').innerHTML=text;
   }
 
@@ -31,12 +28,32 @@ function kikan(arr){
     else{
       const date = new Date(arr[0], arr[1]-1, arr[2]);
       const WeekStr = ['日','月','火','水','木','金','土'];
-      let str = date.getFullYear()
+      let str = (''+date.getFullYear()).slice(-2)
       + '/' + ('0' + (date.getMonth()+1)).slice(-2)
       + '/' + ('0' + date.getDate()).slice(-2)
       +'('+ WeekStr[date.getDay()] +')';
       return str;
       console.log(str);
     }
-
 }
+/*
+function schedule(){
+
+  const limit = CountDown(goal);
+  if(end.length == 3){//終了日入力済
+    document.getElementById("nokori").innerHTML=limit[0];
+    if(limit.some(x => x>0) && limit[0]<10){//残り10日未満 赤
+      document.getElementById("nokori").classList.add("red");
+    }
+    if(limit.every(x => x==0)){//終了済　黒
+      document.getElementById("nokori").classList.remove("red");
+      document.getElementById("finish").innerHTML="終了しました。";
+    }
+  }
+
+  else{//終了日未定
+    document.getElementById("nokori").innerHTML="?";
+  }
+
+  if(limit.some(x => x>0))refresh();
+}*/
